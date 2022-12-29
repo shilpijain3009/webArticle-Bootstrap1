9f40250ef3372b2508081e05018b13fcceb0b9a3
Shilpi <shilpijain3009@gmail.com>
2022-12-29 19:21:29 +0530

    xyz

--- contact.js
+++ contact.js
@@ -1,11 +1,12 @@
-alert("fill form")
-console.log("please fill the form")
-const person={
-    YourName: "Shilpi" ,
-    Lastname: "jain",
-    function : fullname(){
-        return this.YourName+" " + this.Lastname;
-      }
-};
-//document.getElementById("buttonsub").innerHTML = person.fullName();
-<button onclick ="fullname()">Submit</button>
+function fullname() {
+  var x = document.getElementById("nm1").value;
+  var y = document.getElementById("nm2").value;
+  document.getElementById("p1").innerHTML = x ;
+  document.getElementById("p2").innerHTML = y;
+  document.getElementById("p3").innerHTML = x + " " + y;
+  //document.getElementById("lnm").innerHTML = y;
+  console.log(x);
+  console.log(y);
+
+}
+// <button type="button" onclick ="fullname()">Submit</button>
